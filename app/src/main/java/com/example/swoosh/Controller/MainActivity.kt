91ec2232825f5b1d595d65eed8b816110c176e01
eswar <sainath214224@gmail.com>
2020-07-01 19:08:15 +0530

    lesson 05

--- app/src/main/java/com/example/swoosh/Controller/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/MainActivity.kt
@@ -1,8 +1,8 @@
-package com.example.swoosh
+package com.example.swoosh.Controller
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.swoosh.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -12,7 +12,8 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
 
         GetStartedBtn.setOnClickListener {
-            val leagueIntent=Intent(this,LeagueActivity::class.java)
+            val leagueIntent=Intent(this,
+                LeagueActivity::class.java)
             startActivity(leagueIntent)
 
         }
