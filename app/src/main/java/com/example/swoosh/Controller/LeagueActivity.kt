7ef3218bc9bf838ba0e8e7d391faaf0b35f879c7
eswar <sainath214224@gmail.com>
2020-07-02 20:06:49 +0530

    lesson-07

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -12,9 +12,21 @@ import kotlinx.android.synthetic.main.activity_league.*
 class LeagueActivity : BaseActivity(){
 
     var player=Player("","")
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER,player)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState!=null){
+            player= savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onMensClicked(view: View){
